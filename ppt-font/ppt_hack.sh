a6df82ba5d1f7ecbf9450014fd3fef02e61da239
Brandon Amos <brandon.amos.cs@gmail.com>
2023-07-27 09:29:14 -1000

    add scripts

---
+++ ppt-font/ppt_hack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp $PWD/Cambria_hack.ttc /Applications/Microsoft\ PowerPoint.app/Contents/Resources/DFonts/Cambria.ttc
---
+++ ppt-font/ppt_unhack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp $PWD/Cambria_orig.ttc /Applications/Microsoft\ PowerPoint.app/Contents/Resources/DFonts/Cambria.ttc
