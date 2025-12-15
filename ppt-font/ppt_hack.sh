6b6e600afd0f351dfc00ee4184603cdb59904f78
Brandon Amos <brandon.amos.cs@gmail.com>
2025-12-14 21:48:28 -0500

    use SCRIPT_DIR

--- ppt-font/ppt_hack.sh
+++ ppt-font/ppt_hack.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cp $PWD/Cambria_hack.ttc /Applications/Microsoft\ PowerPoint.app/Contents/Resources/DFonts/Cambria.ttc
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cp $SCRIPT_DIR/Cambria_hack.ttc /Applications/Microsoft\ PowerPoint.app/Contents/Resources/DFonts/Cambria.ttc
